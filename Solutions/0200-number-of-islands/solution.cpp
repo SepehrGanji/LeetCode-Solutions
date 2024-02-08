f857fb42b13da4a347fd82c9fb165c03a91910e8
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-02-08 16:24:32 +0000

    [LeetCode Sync] Runtime - 30 ms (49.30%), Memory - 15.8 MB (99.87%)

--- Solutions/0200-number-of-islands/solution.cpp
+++ Solutions/0200-number-of-islands/solution.cpp
@@ -6,54 +6,23 @@ public:
   return true;
 }
 
-void detectMe(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
-  queue<pair<int, int>> qu;
+void detectMe(int i, int j, vector<vector<char>>& grid) {
   int M = grid.size();
   int N = grid[0].size();
-  // M * N
-  qu.push({i, j});
-  while(!qu.empty()) {
-    pair<int, int> pp = qu.front(); qu.pop();
-    int ii = pp.first, jj = pp.second;
-    if(grid[ii][jj] == '1') {
-      visited[ii][jj] = true;
-      if(isValidPoint(ii-1, jj, M, N) && !visited[ii-1][jj]){
-        qu.push({ii - 1, jj});
-        visited[ii-1][jj] = true;
-      }
-
-      if(isValidPoint(ii+1, jj, M, N) && !visited[ii+1][jj]){
-        qu.push({ii + 1, jj});
-        visited[ii+1][jj] = true;
-      }
-
-      if(isValidPoint(ii, jj-1, M, N) && !visited[ii][jj-1]){
-        qu.push({ii, jj - 1});
-        visited[ii][jj-1] = true;
-      }
-
-      if(isValidPoint(ii, jj+1, M, N) && !visited[ii][jj+1]){
-        qu.push({ii, jj + 1});
-        visited[ii][jj+1] = true;
-      }
-    }
-  }
+  grid[i][j] = '0';
+  if(isValidPoint(i-1, j, M, N) && grid[i-1][j] == '1') detectMe(i-1, j, grid);
+  if(isValidPoint(i+1, j, M, N) && grid[i+1][j] == '1') detectMe(i+1, j, grid);
+  if(isValidPoint(i, j-1, M, N) && grid[i][j-1] == '1') detectMe(i, j-1, grid);
+  if(isValidPoint(i, j+1, M, N) && grid[i][j+1] == '1') detectMe(i, j+1, grid);
 }
 
 int numIslands(vector<vector<char>>& grid) {
-  vector<vector<bool>> visited;
-  for(int i = 0 ; i < grid.size() ; i++) {
-    visited.push_back({});
-    for(int j = 0 ; j < grid[i].size() ; j++) {
-      visited[i].push_back(false);
-    }
-  }
   int ans = 0;
   for(int i = 0 ; i < grid.size() ; i++) {
     for(int j = 0 ; j < grid[i].size() ; j++) {
-      if(grid[i][j] == '1' && !visited[i][j]) {
+      if(grid[i][j] == '1') {
         ans++;
-        detectMe(i, j, grid, visited);
+        detectMe(i, j, grid);
       }
     }
   }
