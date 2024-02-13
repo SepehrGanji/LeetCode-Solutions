2c84e377746cc3e6430920e6ac4fb55f828967fe
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-02-13 17:19:20 +0000

    [LeetCode Sync] Runtime - 159 ms (5.81%), Memory - 61.4 MB (5.97%)

--- Solutions/0744-network-delay-time/solution.cpp
+++ Solutions/0744-network-delay-time/solution.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-  k--;
-  // adjacency list
-  vector<vector<pair<int, int>>> g(n);
-  vector<bool> visited(n, false);
-  for(auto e: times) {
-    int a = e[0] - 1;
-    int b = e[1] - 1;
-    int w = e[2];
-    g[a].push_back({b, w});
-  }
-
-  vector<int> dv(n, INT_MAX);
-  visited[k] = true;
-  dv[k] = 0;
-  for(auto el: g[k]) {
-    int b = el.first, w = el.second;
-    dv[b] = w;
-  }
-
-  while(1) {
-    int mini = INT_MAX;
-    int miniel = -1;
-    for(int el = 0 ; el < dv.size() ; el++) {
-      if(visited[el] == false && dv[el] < mini) {
-        mini = dv[el];
-        miniel = el;
+  // Edge list
+  vector<int> distances(n+1, INT_MAX);
+  distances[k] = 0;
+  int iter = n - 1;
+  while(iter--) {
+    bool ch = false;
+    for(auto el: times) {
+      int a = el[0], b = el[1], w = el[2];
+      if(distances[a] < INT_MAX && distances[a] + w < distances[b]) {
+        distances[b] = distances[a] + w;
+        ch = true;
       }
     }
-    if(miniel == -1) break;
-
-    // now we have the minimum distance, so update!
-    visited[miniel] = true;
-    for(auto el: g[miniel]) {
-      int b = el.first, w = el.second;
-      if(!visited[b] && dv[miniel] + w < dv[b]) {
-        dv[b] = dv[miniel] + w;
-      }
-    }
+    if(!ch) break;
   }
-
   int maxim = -1;
-  for(auto el: dv) {
-    if(el > maxim) maxim = el;
+  for(int i = 1 ; i <= n ; i++) {
+    if(distances[i] > maxim) maxim = distances[i];
   }
-  return maxim == INT_MAX ? -1 : maxim;
+  return maxim < INT_MAX ? maxim : -1;
 }
 };
