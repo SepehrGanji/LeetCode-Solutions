0cc1a08a974a2ebd71166ef19c5d08db48382577
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-09-05 15:20:32 +0000

    [LeetCode Sync] Runtime - 4 ms (16.20%), Memory - 9.4 MB (26.97%)

--- Solutions/0125-valid-palindrome/solution.cpp
+++ Solutions/0125-valid-palindrome/solution.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-  if(s.length() <= 1) return true;
-  string fin = "";
-  for(int i = 0; i < s.length() ; i++) {
-    char c = s[i];
-    if(c >= 'a' && c <= 'z') fin += c;
-    else if(c >= 'A' && c <= 'Z') fin += (c - 'A') + 'a';
-    else if(c >='0' && c <= '9') fin +=c; 
-  }
-  // cout << fin << endl;
-  if(fin.length() <= 1) return true;
-  int p1 = 0, p2 = fin.length() - 1;
-  while(p1 < p2) {
-    if(fin[p1] != fin[p2]) return false;
-    p1++;p2--;
-  }
-  return true;
+    string str = "";
+    for(int i = 0 ; i < s.length() ; i++) {
+        char c = s[i];
+        if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) str += c;
+        if(c >= 'A' && c <= 'Z') str += 'a' + (c - 'A');
+    }
+    int lptr = 0, rptr = str.length()-1;
+    while(lptr < rptr) {
+        if(str[lptr] != str[rptr]) return false;
+        lptr++, rptr--;
+    }
+    return true;
 }
 };
