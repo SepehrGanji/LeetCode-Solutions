6c78f86f980b519e4f9fe83a4db9a7ac342e3c62
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-09-10 22:17:23 +0000

    [LeetCode Sync] Runtime - 10 ms (10.86%), Memory - 21.1 MB (7.29%)

---
+++ Solutions/0135-candy/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    vector<int> cand;
+    for(int i = 0 ; i < n ; i++) cand.push_back(1);
+    for(int i = 1 ; i < n ; i++) if(ratings[i] > ratings[i-1]) cand[i] = cand[i-1] + 1;
+    for(int i = n - 2 ; i >= 0 ; i--) if(ratings[i] > ratings[i+1]) cand[i] = max(cand[i], cand[i+1]+1);
+    int ans = 0;
+    for(auto el: cand) ans += el;
+    return ans;
+}
+};
