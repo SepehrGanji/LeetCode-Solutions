11887cb748209129089f1dfe93c0626329ae6b93
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-09-18 16:42:14 +0000

    [LeetCode Sync] Runtime - 4 ms (34.44%), Memory - 19.3 MB (80.82%)

---
+++ Solutions/0045-jump-game-ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if (nums.size() <= 1) return 0;
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            nums[i] = max(nums[i] + i, nums[i - 1]);
+        }
+        
+        int currentIndex = 0;
+        int jumps = 0;
+        
+        while (currentIndex < nums.size() - 1) {
+            jumps++;
+            currentIndex = nums[currentIndex];
+        }
+        
+        return jumps;
+    }
+};
