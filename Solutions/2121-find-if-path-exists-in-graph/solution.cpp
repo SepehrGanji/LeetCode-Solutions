4c93142e8cba9b0479a3329e2c31e183162b8795
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-10-15 21:59:55 +0000

    [LeetCode Sync] Runtime - 857 ms (30.35%), Memory - 343.3 MB (37.09%)

---
+++ Solutions/2121-find-if-path-exists-in-graph/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>> gr;
+        vector<bool> visited;
+        for(int i = 0 ; i < n ; i++) {
+            gr.push_back({});
+            visited.push_back(false);
+        }
+        for(auto edge: edges) {
+            int a = edge[0], b = edge[1];
+            gr[a].push_back(b);
+            gr[b].push_back(a);
+        }
+        stack<int> st;
+        st.push(source);
+        while(!st.empty()) {
+            int top = st.top(); st.pop();
+            if(!visited[top]) {
+                visited[top] = true;
+                if(top == destination) return true;
+                for(auto v: gr[top]) {
+                    st.push(v);
+                }
+            }
+        }
+        
+        return false;
+    }
+};
