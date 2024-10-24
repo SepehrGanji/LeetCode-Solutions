5a9b5e38c2104802ce916c0a28a557fdfcfe5d5e
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-10-24 16:06:59 +0000

    [LeetCode Sync] Runtime - 1 ms (67.58%), Memory - 10.3 MB (35.14%)

--- Solutions/0482-license-key-formatting/solution.cpp
+++ Solutions/0482-license-key-formatting/solution.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        string str = "";
-        for(auto ch: s) {
-            if(ch != '-') {
-                if(ch >= 'a' && ch <= 'z') {
-                    str += ('A' + (ch - 'a'));
-                } else str += ch;
-            }
+        string raw = "";
+        for(auto ch: s) if(ch != '-') {
+            if(ch >= 'a' && ch <= 'z') raw += 'A' + (ch - 'a');
+            else raw += ch;
         }
+        int n = raw.length();
+        int off = 0;
         string ans = "";
-        int rem = str.length() % k;
-        if(rem > 0) {
-            for(int i = 0 ; i < rem ; i++) ans += str[i];
-            if(str.length() > rem) ans += '-';
+        if(n % k > 0) {
+            int m = n % k;
+            while(off < m) ans += raw[off++];
+            if(off < n) ans += '-';
         }
-        for(int i = 0 ; i < str.length() - rem ; i++) {
-            ans += str[rem+i];
-            if(i < str.length() - rem-1 && i % k == k-1) ans += '-';
+        for(int i = 0 ; i+off < n ; i++) {
+            ans += raw[i+off];
+            if((i+1) % k == 0 && i+off != n-1) ans += '-';
         }
+        
         return ans;
     }
 };
