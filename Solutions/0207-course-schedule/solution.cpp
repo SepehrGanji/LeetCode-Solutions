5f022836e8cb2d42600cc1ab60e323e3155545a4
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-11-25 16:04:13 +0000

    [LeetCode Sync] Runtime - 11 ms (26.92%), Memory - 19.9 MB (26.76%)

--- Solutions/0207-course-schedule/solution.cpp
+++ Solutions/0207-course-schedule/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-  int n = numCourses;
-  vector<vector<int>> g;
-  vector<int> degree(n, 0);
-  stack<int> st;
-  for(int i = 0 ; i < n ; i++) g.push_back({});
-  for(auto record: prerequisites) {
-    // a -> b
-    int b = record.at(0);
-    int a = record.at(1);
-    g[a].push_back(b);
-    degree[b]++;
-  }
-  for(int i = 0 ; i < n ; i++) if(degree[i] == 0) st.push(i);
-  int cnt = 0;
-  while(!st.empty()) {
-    cnt++;
-    int curr = st.top(); st.pop();
-    for(auto el: g[curr]) {
-      if(--degree[el] == 0) st.push(el);
-    }
-  }
+        int n = numCourses;
+        vector<vector<int>> g;
+        vector<int> degree(n, 0);
+        stack<int> st;
+        for(int i = 0 ; i < n ; i++) g.push_back({});
+        for(auto record: prerequisites) {
+            // a -> b
+            int b = record.at(0);
+            int a = record.at(1);
+            g[a].push_back(b);
+            degree[b]++;
+        }
+        for(int i = 0 ; i < n ; i++) if(degree[i] == 0) st.push(i);
+        int cnt = 0;
+        while(!st.empty()) {
+            cnt++;
+            int curr = st.top(); st.pop();
+            for(auto el: g[curr]) {
+                if(--degree[el] == 0) st.push(el);
+            }
+        }
 
-  return cnt == n;
-}
+        return cnt == n;
+    }
 };
