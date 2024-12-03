32f9957c69f412a887ac863b56ac4959616f772e
Sepehr (Simon) <sepehrganji79@gmail.com>
2024-12-03 23:00:07 +0000

    [LeetCode Sync] Runtime - 104 ms (13.95%), Memory - 48.9 MB (11.57%)

--- Solutions/0744-network-delay-time/solution.cpp
+++ Solutions/0744-network-delay-time/solution.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+    typedef pair<int, int> pii;
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-  // Edge list
-  vector<int> distances(n+1, INT_MAX);
-  distances[k] = 0;
-  int iter = n - 1;
-  while(iter--) {
-    bool ch = false;
-    for(auto el: times) {
-      int a = el[0], b = el[1], w = el[2];
-      if(distances[a] < INT_MAX && distances[a] + w < distances[b]) {
-        distances[b] = distances[a] + w;
-        ch = true;
-      }
+        vector<bool> visited(n, false);
+        vector<vector<pii>> gr(n);
+        for(auto ed: times) {
+            int u = ed[0], v = ed[1], w = ed[2];
+            gr[u-1].push_back(pii(v-1, w));
+        }
+        int maxTime = 0;
+        priority_queue<pii, vector<pii>, greater<>> pq;
+        pq.push(pii(0, k-1));
+        while(!pq.empty()) {
+            auto el = pq.top(); pq.pop();
+            int t = el.first, node = el.second;
+            if(visited[node]) continue;
+            visited[node] = true;
+            maxTime = max(maxTime, t);
+            for(auto ed: gr[node]) {
+                if(!visited[ed.first]) {
+                    pq.push(pii(t + ed.second, ed.first));
+                }
+            }
+        }
+        for(int i = 0 ; i < n ; i++) if(visited[i] == false) return -1;
+        return maxTime;
     }
-    if(!ch) break;
-  }
-  int maxim = -1;
-  for(int i = 1 ; i <= n ; i++) {
-    if(distances[i] > maxim) maxim = distances[i];
-  }
-  return maxim < INT_MAX ? maxim : -1;
-}
 };
