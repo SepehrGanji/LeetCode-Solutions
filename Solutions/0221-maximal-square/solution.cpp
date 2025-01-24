2b822a7342b3f1c2db13eb64da9e5851527f67d9
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-01-24 15:37:21 +0000

    [LeetCode Sync] Runtime - 7 ms (68.73%), Memory - 31.1 MB (34.25%)

--- Solutions/0221-maximal-square/solution.cpp
+++ Solutions/0221-maximal-square/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int rows = matrix.size(), cols = rows > 0 ? matrix[0].size() : 0;
-        vector<vector<int>> dp(rows + 1, vector<int>(cols + 1, 0));
-        int maxsqlen = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i - 1][j - 1] == '1') {
-                    dp[i][j] =
-                        min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) +
-                        1;
-                    maxsqlen = max(maxsqlen, dp[i][j]);
-                }
-            }
-        }
-        return maxsqlen * maxsqlen;
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < m ; j++) dp[i][j] = matrix[i][j] - '0';
+        ///
+        for(int i = 1 ; i < n ; i++)
+            for(int j = 1 ; j < m ; j++)
+                if(dp[i][j] == 1) dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+        ///
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < m ; j++) ans = max(ans, dp[i][j]);
+        return ans*ans;
     }
 };
