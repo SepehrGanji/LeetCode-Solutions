109b629abee069c94e98b129eac4edeb74ace08e
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-03-22 17:22:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.6 MB (68.03%)

---
+++ Solutions/0861-flipping-an-image/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        vector<vector<int>> ans(n, vector<int>(n, 0));
+        for(int i = 0 ; i < n ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                ans[i][j] = 1 - image[i][n-j-1];
+            }
+        }
+        
+        return ans;
+    }
+};
