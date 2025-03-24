4d7bf625ceab173c3813dd7ec909aaaebac53e72
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-03-24 17:28:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 70 MB (96.21%)

--- Solutions/0940-fruit-into-baskets/solution.cpp
+++ Solutions/0940-fruit-into-baskets/solution.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // Hash map 'basket' to store the types of fruits.
-        unordered_map<int, int> basket;
-        int left, right;
+        int ans = 1;
+        int A = fruits[0], fst_A = 0, lst_A = 0;
+        int B = -1, fst_B = -1, lst_B = -1;
         
-        // Add fruit from the right index (right) of the window.
-        for (left = 0, right = 0; right < fruits.size(); ++right) {
-            basket[fruits[right]]++;
-            
-            // If the current window has more than 2 types of fruit,
-            // we remove one fruit from the left index (left) of the window.
-            if (basket.size() > 2) {
-                basket[fruits[left]]--;
-                if (basket[fruits[left]] == 0)
-                    basket.erase(fruits[left]);
-                left++;
+        int l = 0, r = 1; // r is the next candiate
+        while(r < fruits.size()) {
+            int cf = fruits[r];
+            if(cf == A || cf == B) {
+                if(cf == A) lst_A = r;
+                else lst_B = r;
+                
+                ans = max(ans, r-l+1);
+                r++;
+            } else {
+                if(B == -1) {
+                    B = cf;
+                    // fst_B = r;
+                    lst_B = r;
+                    ans = max(ans, r-l+1);
+                    r++;
+                } else {
+                    if(lst_A < lst_B) {
+                        // replace A, window now is containing B
+                        l = lst_A + 1;
+                        A = cf;
+                        lst_A = r;
+                        ans = max(ans, r-l+1);
+                        r++;
+                    } else {
+                        l = lst_B + 1;
+                        B = cf;
+                        lst_B = r;
+                        ans = max(ans, r-l+1);
+                        r++;
+                    }
+                }
             }
         }
-        
-        // Once we finish the iteration, the indexes left and right 
-        // stands for the longest valid subarray we encountered.
-        return right - left;
+        return ans;
     }
 };
