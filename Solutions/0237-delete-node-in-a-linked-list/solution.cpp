fcb5331bfa77d2c5eb0f605fe989a9bb452dafca
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-04-04 17:46:28 +0000

    [LeetCode Sync] Runtime - 10 ms (33.06%), Memory - 12.4 MB (53.03%)

---
+++ Solutions/0237-delete-node-in-a-linked-list/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        while(1) {
+            node->val= node->next->val;
+            if(node->next->next == nullptr) {
+                node->next = nullptr;
+                break;
+            }
+            node = node->next;
+        }
+    }
+};
