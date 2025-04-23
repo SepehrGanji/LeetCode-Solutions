cdffb3bd05586dbf3e7ceda4355d3f99e3ad6c4c
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-04-23 17:22:34 +0000

    [LeetCode Sync] Runtime - 2 ms (86.43%), Memory - 15.6 MB (13.50%)

--- Solutions/0003-longest-substring-without-repeating-characters/solution.cpp
+++ Solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         if(s == "") return 0;
-        int mp[1000];
-        for(int i = 0 ; i < 1000 ; i++) mp[i] = -1;
-        int l = 0, r = 1;
-        mp[s[0]] = 0;
         int ans = 1;
+        vector<int> mp(1000, -1);
+        int l = 0, r = 1; // r is the next candidate
+        mp[s[0]] = 0;
         while(r < s.length()) {
-            // r is the next candidate
-            if(mp[s[r]] >= l) {
-                // can't, sorry, already seen!
+            if(mp[s[r]] < l) {
+                // OK to add
+                ans = max(ans, r-l+1);
+            } else {
+                // not OK to add
                 l = mp[s[r]] + 1;
+                ans = max(ans, r-l+1);
             }
             mp[s[r]] = r;
-                ans = max(ans, r-l+1);
             r++;
         }
-        
         return ans;
     }
 };
