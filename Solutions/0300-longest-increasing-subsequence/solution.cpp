199445c0ac7c261b19b539d0d9d625c4a1f992de
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-04-24 15:32:54 +0000

    [LeetCode Sync] Runtime - 51 ms (69.02%), Memory - 14.6 MB (38.81%)

--- Solutions/0300-longest-increasing-subsequence/solution.cpp
+++ Solutions/0300-longest-increasing-subsequence/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-  int dp[2525];
-  int n = nums.size();
-  for(int i = 0 ; i < n ; i++) dp[i] = 1;
-  for(int i = 1 ; i < n ; i++) {
-    int maxim = 1;
-    for(int j = 0 ; j < i ; j++) {
-      if(nums[j] < nums[i] && dp[j] + 1 > maxim) 
-        maxim = dp[j] + 1;
+      int dp[2525];
+      int n = nums.size();
+      for(int i = 0 ; i < n ; i++) dp[i] = 1;
+      for(int i = 1 ; i < n ; i++) {
+        int maxim = 1;
+        for(int j = 0 ; j < i ; j++) {
+          if(nums[j] < nums[i] && dp[j] + 1 > maxim) 
+            maxim = dp[j] + 1;
+        }
+        dp[i] = maxim;
+      }
+
+      return *max_element(dp, dp+n);
     }
-    dp[i] = maxim;
-  }
-  
-  return *max_element(dp, dp+n);
-}
 };
