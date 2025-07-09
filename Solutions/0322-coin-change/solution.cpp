ea0619f86405c1649866cde4e3664069ff808497
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-07-09 17:42:10 +0000

    [LeetCode Sync] Runtime - 1612 ms (5.03%), Memory - 17.8 MB (84.28%)

--- Solutions/0322-coin-change/solution.cpp
+++ Solutions/0322-coin-change/solution.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-  int dp[10010];
-  for(int i = 0 ; i <= amount ; i++) {
-    dp[i] = INT_MAX-1;
-  }
-  dp[0] = 0;
-  for(int i = 1 ; i <= amount ; i++) {
-    for(auto c: coins) {
-      if(i - c >= 0) {
-        dp[i] = min(dp[i], dp[i-c] + 1);
-      }
+        vector<int> ans(amount+1, -1);
+        ans[0] = 0;
+        for(int i = 1 ; i <= amount ; i++) {
+            int cur_min = INT_MAX;
+            for(auto c: coins) {
+                int idx = i - c;
+                if(idx < 0) continue;
+                if(ans[idx] == -1) continue;
+                cur_min = min(cur_min, 1+ans[idx]);
+            }
+            if(cur_min < INT_MAX) ans[i] = cur_min;
+        }
+        for(int i = 0 ; i <= amount ; i++) cout << ans[i] << ' ';
+        cout << endl;
+        return ans[amount];
     }
-  }
-
-  return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
-}
 };
