f921c05837f210be3a36304ceaa114a00a4aee1d
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-07-11 19:39:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (37.47%)

--- Solutions/0278-first-bad-version/solution.cpp
+++ Solutions/0278-first-bad-version/solution.cpp
@@ -4,10 +4,10 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long long l = 1, r = n;
+        int l = 1, r = n;
         while(l < r) {
-            long long mid = (l+r)/2;
-            if(isBadVersion(mid)) r=mid;
+            int mid = l + (r-l)/2;
+            if(isBadVersion(mid)) r = mid;
             else l = mid+1;
         }
         return r;
