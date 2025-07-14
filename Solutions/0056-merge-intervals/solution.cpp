77191c9ea32012d22c7b10fd39f9691d56712e42
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-07-14 16:01:20 +0000

    [LeetCode Sync] Runtime - 4 ms (69.36%), Memory - 24.3 MB (21.16%)

--- Solutions/0056-merge-intervals/solution.cpp
+++ Solutions/0056-merge-intervals/solution.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<pair<int, int>> arr;
-        for(auto el: intervals) arr.push_back(make_pair(el[0], el[1]));
-        sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
-        ans.push_back({arr[0].first, arr[0].second});
-        for(int i = 1 ; i < arr.size() ; i++) {
-            auto curr_el = arr[i];
-            int a = curr_el.first, b = curr_el.second;
-            auto last_el = ans.back();
-            int aa = last_el[0], bb = last_el[1];
-            if(a <= bb) {
-                ans.pop_back();
-                ans.push_back({min(a, aa), max(b, bb)});
-            } else {
-                ans.push_back({a, b});
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        for(int i = 0 ; i < n ; ) {
+            int s = intervals[i][0], e = intervals[i][1];
+            i++;
+            while(i < n && intervals[i][0] <= e) {
+                e = max(intervals[i][1], e);
+                i++;
             }
+            vector<int> veve = {s, e};
+            ans.push_back(veve);
         }
         return ans;
     }
