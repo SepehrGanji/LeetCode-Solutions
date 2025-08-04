e1d8dbedbbdd8507834b12fbb67c3946c43590a8
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-08-04 15:29:37 +0000

    [LeetCode Sync] Runtime - 40 ms (52.45%), Memory - 75 MB (53.60%)

--- Solutions/0940-fruit-into-baskets/solution.cpp
+++ Solutions/0940-fruit-into-baskets/solution.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int ans = 1;
-        int A = fruits[0], fst_A = 0, lst_A = 0;
-        int B = -1, fst_B = -1, lst_B = -1;
-        
-        int l = 0, r = 1; // r is the next candiate
-        while(r < fruits.size()) {
-            int cf = fruits[r];
-            if(cf == A || cf == B) {
-                if(cf == A) lst_A = r;
-                else lst_B = r;
-                
-                ans = max(ans, r-l+1);
-                r++;
-            } else {
-                if(B == -1) {
-                    B = cf;
-                    // fst_B = r;
-                    lst_B = r;
-                    ans = max(ans, r-l+1);
-                    r++;
-                } else {
-                    if(lst_A < lst_B) {
-                        // replace A, window now is containing B
-                        l = lst_A + 1;
-                        A = cf;
-                        lst_A = r;
-                        ans = max(ans, r-l+1);
-                        r++;
-                    } else {
-                        l = lst_B + 1;
-                        B = cf;
-                        lst_B = r;
-                        ans = max(ans, r-l+1);
-                        r++;
-                    }
-                }
+        unordered_map<int, int> basket;
+        int left, right;
+
+        for (left = 0, right = 0; right < fruits.size(); ++right) {
+            basket[fruits[right]]++;
+            if (basket.size() > 2) {
+                basket[fruits[left]]--;
+                if (basket[fruits[left]] == 0)
+                    basket.erase(fruits[left]);
+                left++;
             }
         }
-        return ans;
+        return right - left;
     }
 };
