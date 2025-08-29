ea5a3ca94b5fc578bab52d84e85c864e743138bc
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-08-29 18:34:26 +0000

    [LeetCode Sync] Runtime - 79 ms (46.97%), Memory - 10.3 MB (61.21%)

--- Solutions/0005-longest-palindromic-substring/solution.cpp
+++ Solutions/0005-longest-palindromic-substring/solution.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-  int n = s.length();
-  bool dp[1010][1010];
-  for(int i = 0 ; i < n ; i++)
-    for(int j = 0 ; j < n ; j++)
-      dp[i][j] = false;
-  
-  for(int i = 0 ; i < n ; i++) {
-    dp[i][i] = true;
-    if(i < n - 1 && s[i] == s[i+1]) {
-      dp[i][i+1] = true;
-    }
-  }
+    bool dp[1010][1010];
+    // string ans[1010][1010];
+    pair<int, int> ans;
 
-  for(int L = 2 ; L < n ; L++) {
-    for(int i = 0 ; i < n ; i++) {
-      int j = i + L;
-      if(j >= n) break;
-      if(s[i] == s[j] && dp[i+1][j-1] == true)
-        dp[i][j] = true;
-    }
-  }
-
-  int maxL = -1;
-  int maxi, maxj;
-  for(int i = 0 ; i < n ; i++) {
-    for(int j = 0 ; j < n ; j++) {
-      if(dp[i][j]) {
-        if(j - i > maxL) {
-          maxL = j - i;
-          maxi = i;
-          maxj = j;
+    string longestPalindrome(string s) {    
+        int n = s.length();
+        for(int i = 0 ; i < n ; i++) dp[i][i] = true;
+        for(int i = 0 ; i < n-1 ; i++) {
+            if(s[i] == s[i+1]) {
+                dp[i][i+1] = true;
+                ans = {i, i+1};
+            }
+            
         }
-      }
-    }
-  }
-  string ans = "";
-  for(int i = maxi ; i <= maxj ; i++)
-    ans += s[i];
 
-  return ans;
-}
+        for(int L = 2 ; L < n ; L++) {
+            for(int i = 0 ; i < n - L ; i++) {
+                int j = i + L;
+                if(s[i] == s[j] && dp[i+1][j-1]) {
+                    dp[i][j] = true;
+                    ans = {i, j};
+                }
+            }
+        }
+
+        return s.substr(ans.first, ans.second - ans.first + 1);
+    }
 };
+
