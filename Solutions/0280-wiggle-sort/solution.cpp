9827380d1945c0243e249ae7e626d261b4ff1ab9
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-09-10 14:09:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (31.03%)

--- Solutions/0280-wiggle-sort/solution.cpp
+++ Solutions/0280-wiggle-sort/solution.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<int> ans;
-        int pk = n-1;
-        int i = 0;
-        
-        while(i < pk) {
-            ans.push_back(nums[i]);
-            ans.push_back(nums[pk]);
-            // ans.push_back(nums[i+1]);
-            i++;
-            pk--;
-        }
-        if(i == pk) ans.push_back(nums[i]);
-        for(int i = 0 ; i < n ; i++) nums[i] = ans[i];
+        for(int i = 1 ; i < nums.size()-1 ; i += 2) swap(nums[i], nums[i+1]);
     }
 };
