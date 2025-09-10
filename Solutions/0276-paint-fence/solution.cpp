4c0b287fc4dd3a9983eb7a17b9de3682c429d8e3
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-09-10 14:03:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (63.11%)

---
+++ Solutions/0276-paint-fence/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWays(int n, int k) {
+        if(n == 1) return k;
+        if(n == 2) return k*k;
+
+        int dp[55];
+        dp[1] = k;
+        dp[2] = k * k;
+        for(int i = 3 ; i <= n ; i++) {
+            dp[i] = (k-1) * (dp[i-1] + dp[i-2]);
+        }
+        return dp[n];
+    }
+};
+
