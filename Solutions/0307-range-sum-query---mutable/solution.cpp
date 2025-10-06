ee20917ecf863c9e5205a5dd9454811a7a6e80c3
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-10-06 19:54:09 +0000

    [LeetCode Sync] Runtime - 59 ms (63.86%), Memory - 182.4 MB (41.53%)

--- Solutions/0307-range-sum-query---mutable/solution.cpp
+++ Solutions/0307-range-sum-query---mutable/solution.cpp
@@ -1,54 +1,37 @@
 class NumArray {
-public:
-    vector<int> tree; int n;
-    
-    NumArray(vector<int>& nums) {
-        n = nums.size();
-        tree.resize(4 * n);
-        buildTree(nums, 0, 0, n-1);
-    }
-    
-    void update(int index, int val) {
-        updateTree(0, 0, n - 1, index, val);
-    }
-    
-    int sumRange(int left, int right) {
-        return queryTree(0, 0, n - 1, left, right);
-    }
-    
-    void buildTree(vector<int> &nums, int treeIdx, int l, int r) {
-        if(l == r) {
-            tree[treeIdx] = nums[l];
+private:
+    vector<int> tree;
+    int n;
+
+    void buildtree(vector<int>& nums, int treeIdx, int lo, int hi) {
+        if(lo == hi) {
+            tree[treeIdx] = nums[lo];
             return;
         }
-        int mid = (l + r) / 2;
-        int left_child = 2 * treeIdx + 1;
-        int right_child = 2 * treeIdx + 2;
-        
-        buildTree(nums, left_child, l, mid);
-        buildTree(nums, right_child, mid+1, r);
-        
-        tree[treeIdx] = tree[left_child] + tree[right_child];
+        int mid = (lo + hi) / 2;
+        buildtree(nums, 2*treeIdx+1, lo, mid);
+        buildtree(nums, 2*treeIdx+2, mid+1, hi);
+        tree[treeIdx] = tree[2*treeIdx+1] + tree[2*treeIdx+2];
     }
-    
-    int queryTree(int treeIndex, int low, int high, int i, int j) {
-        if(low > j || high < i) return 0;
-        if(low >= i && high <= j) return tree[treeIndex];
-        
-        int mid = (low + high) / 2;
-        int left_child = 2 * treeIndex + 1;
-        int right_child = 2 * treeIndex + 2;
-        
-        return queryTree(left_child, low, mid, i, j) + queryTree(right_child, mid+1, high, i, j);
+
+    int querytree(int treeIndex, int lo, int hi, int i, int j) {
+        if (lo > j || hi < i)
+            return 0; 
+
+        if (i <= lo && j >= hi)
+            return tree[treeIndex];
+
+        int mid = (lo + hi) / 2;
+        return querytree(2 * treeIndex + 1, lo, mid, i, j) + querytree(2 * treeIndex + 2, mid+1, hi, i, j);
     }
-    
+
     void updateTree(int treeIndex, int lo, int hi, int arrIndex, int val) {
         if(lo == hi) {
             tree[treeIndex] = val;
             return;
         }
         
-        int mid = lo + (hi - lo) / 2;
+        int mid = (lo+hi)/2;
         int leftChild = 2 * treeIndex + 1;
         int rightChild = 2 * treeIndex + 2;
         if (arrIndex <= mid) {
@@ -59,6 +42,21 @@ public:
             updateTree(rightChild, mid + 1, hi, arrIndex, val);
         }
         tree[treeIndex] = tree[leftChild] + tree[rightChild];
+    }
+
+public:
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        tree.resize(n * 4);
+        buildtree(nums, 0, 0, n-1);
+    }
+    
+    void update(int index, int val) {
+        updateTree(0, 0, n-1, index, val);
+    }
+    
+    int sumRange(int left, int right) {
+        return querytree(0, 0, n-1, left, right);
     }
 };
 
