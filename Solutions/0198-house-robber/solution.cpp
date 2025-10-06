0ebd6f9c1bbbeea9238228613061e4710cf950c4
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-10-06 16:20:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (5.87%)

--- Solutions/0198-house-robber/solution.cpp
+++ Solutions/0198-house-robber/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-      int n = nums.size();
-      if(n == 1) return nums[0];
-      else if(n == 2) return max(nums[0], nums[1]);
-      vector<int> dp(n, 0);
-      int mamad = max(nums[0], nums[1]);
-      dp[0] = nums[0], dp[1] = mamad; 
-      for(int i = 2 ; i < n ; i++) {
-        dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-      }
-      return dp[n-1];
+        if(nums.size() < 2) return nums[0];
+        vector<int> dp;
+        dp.resize(nums.size());
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i = 2 ; i < nums.size() ; i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[nums.size()-1];
     }
 };
