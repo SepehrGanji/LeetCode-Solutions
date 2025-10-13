b0dc0ceae4301fa448879bef8e706eb2bf63da95
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-10-13 15:09:27 +0000

    [LeetCode Sync] Runtime - 9 ms (49.21%), Memory - 39.3 MB (21.79%)

---
+++ Solutions/0359-logger-rate-limiter/solution.cpp
@@ -0,0 +1,20 @@
+class Logger {
+public:
+    unordered_map<string, int> mp;
+    Logger() {}
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        if(mp.find(message) == mp.end() || mp[message] + 10 <= timestamp) {
+            mp[message] = timestamp;
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
