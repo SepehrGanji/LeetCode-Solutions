94ccad296c434d4d2e8d4506ff3a1f5e230724f6
Sepehr (Simon) <sepehrganji79@gmail.com>
2025-12-22 22:29:34 +0000

    [LeetCode Sync] Runtime - 54 ms (91.66%), Memory - 172.8 MB (95.18%)

--- Solutions/0146-lru-cache/solution.cpp
+++ Solutions/0146-lru-cache/solution.cpp
@@ -1,74 +1,58 @@
-class LRUCache {
 struct Node {
-    int val;
-    int key;
     Node* next;
     Node* prev;
-    Node(int key, int val): key(key), val(val), next(nullptr), prev(nullptr) {}
+    int val; 
+    int key;
 };
 
-public:
+class LRUCache {
+private:
     Node* head = nullptr;
     Node* tail = nullptr;
-    map<int, Node*> mp;
+    unordered_map<int, Node*> mp;
     int cap;
+public:
     LRUCache(int capacity) {
-        cap = capacity;
+        this->cap = capacity;
     }
     
     int get(int key) {
-        // accessed! so move to the back please
         if(mp.find(key) == mp.end()) return -1;
-        auto el_to_move = mp[key];
-        if(el_to_move == tail) {
-            return mp[key]->val;
-        }
-        if(el_to_move == head) {
-            head = head->next;
-            head->prev = nullptr;
-            el_to_move->next = nullptr;
-            tail->next = el_to_move;
-            el_to_move->prev = tail;
-            tail = el_to_move;
-            return mp[key]->val;
-        }
-        
-        if(el_to_move->prev != nullptr) el_to_move->prev->next = el_to_move->next;
-        if(el_to_move->next != nullptr) el_to_move->next->prev = el_to_move->prev;
-        tail->next = el_to_move;
-        el_to_move->prev = tail;
-        el_to_move->next = nullptr;
-        tail = el_to_move;
-        return mp[key]->val;
+        Node* curr = mp[key];
+        if(curr == tail) return curr->val;
+        // what if curr is head?!
+        if(curr->prev != nullptr) curr->prev->next = curr->next;
+        if(curr->next != nullptr) curr->next->prev = curr->prev;
+        if(curr == head) head = head ->next;
+        tail->next = curr;
+        curr->prev = tail;
+        tail = curr;
+        return curr->val;
     }
     
     void put(int key, int value) {
+        // do we have to move when update?!
         if(get(key) != -1) {
             mp[key]->val = value;
             return;
         }
-        if(mp.size() == 0) {
-            // init!
-            head = new Node(key, value);
-            tail = head;
-            mp[key] = head;
+        Node* curr = new Node();
+        mp[key] = curr;
+        curr->val = value;
+        curr->key = key;
+        if(head == nullptr) {
+            head = curr;
+            tail = curr;
             return;
         }
-
-        // add to the thing
-        Node* newNode = new Node(key, value);
-        mp[key] = newNode;
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
-
+        tail->next = curr;
+        curr->prev = tail;
+        tail = curr;
         if(mp.size() > cap) {
-            // delete!
-            // Node* oldhead = head;
+            // evict!
             mp.erase(head->key);
             head = head->next;
             head->prev = nullptr;
-            // delete oldhead;
         }
     }
 };
