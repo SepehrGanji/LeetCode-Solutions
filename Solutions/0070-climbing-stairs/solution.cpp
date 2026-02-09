e323665972904f0c12be2dfd40231f258159b321
Sepehr (Simon) <sepehrganji79@gmail.com>
2026-02-09 16:07:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (54.74%)

--- Solutions/0070-climbing-stairs/solution.cpp
+++ Solutions/0070-climbing-stairs/solution.cpp
@@ -3,9 +3,11 @@ public:
     int climbStairs(int n) {
         if(n == 1) return 1;
         if(n == 2) return 2;
-        int dp[n];
-        dp[0] = 1; dp[1] = 2;
-        for(int i = 2 ; i < n ; i++) dp[i] = dp[i-1] + dp[i-2];
-        return dp[n-1];
+        int arr[100];
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i = 3 ; i <= n ; i++) arr[i] = arr[i-1] + arr[i-2];
+        return arr[n];
     }
 };
+
