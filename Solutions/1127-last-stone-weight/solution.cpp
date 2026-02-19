6507a1411ad1eee6022a2be745f6c67e4bb303b4
Sepehr (Simon) <sepehrganji79@gmail.com>
2026-02-19 21:01:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.9 MB (78.03%)

---
+++ Solutions/1127-last-stone-weight/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(auto el: stones) pq.push(el);
+        while(pq.size() > 1) {
+            int y = pq.top() ; pq.pop();
+            int x = pq.top() ; pq.pop();
+            if(x != y) pq.push(y-x);
+        }
+        if(pq.empty()) return 0;
+        return pq.top();
+    }
+};
